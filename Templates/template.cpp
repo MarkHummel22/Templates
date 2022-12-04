efb50cf40ba633b34eb49bc7219b85b06ee35bbe
Mark <hummel.marcus@icloud.com>
2022-12-04 01:02:15 -0500

    staples button *press*

--- Templates/template.cpp
+++ Templates/template.cpp
@@ -3,11 +3,29 @@
 // 12/3/22
 
 #include <iostream>
+#include <iomanip>
+#include <cmath>
 
 using namespace std;
 
-int main() {
+template <typename T>
+double half(T number) {
+	return number / 2.0;
+}
 
+int main() 
+{
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
+
+	cout << setprecision(2) << showpoint;
+
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << ceil(half(c)) << endl << endl;
+
+	system("pause");
 
 
 	return 0;
